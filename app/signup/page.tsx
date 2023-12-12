65bd8d8d721cbd2e6db4ad752663a5e86cd8c926
Amit Kumar <amitgodara1008@gmail.com>
2023-12-12 12:22:24 +0530

    remove footer from signup/signin

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -17,21 +17,7 @@ export default function signup() {
         <div className="py-9 h-auto w-1/3">
         <SignupBox />
         </div>
-        
       </div>
-      <div className="mb-10" style={{ backgroundImage: "url(https://rx.link/assets/img/call-to-action-bg.svg)" }}>
-        <div className="container text-center p-10">
-          <h2 className="text-dark text-3xl font-bold py-3 my-0 pt-0 pb-3">It&apos;s easy to get Started</h2>
-          <p className="font-medium text-base pb-2">And it&apos;s free. Two things everyone loves.</p>
-          <div className="mt-5">
-            <a href="#" className="text-white text-sm font-bold bg-blue-500 rounded-full py-2 px-6 no-underline inline-block">
-              Get Started
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <Footer />
     </div>
   )
 }
